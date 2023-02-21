c5f2d63d4bd19efa6031b8200cdd72575ec6a262
Jemisson <jemison321@gmail.com>
2023-02-21 13:50:56 -0400

    ajustes de frontend na index do user e criação de enums para role do profile admin.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,8 @@ import "controllers"
 import "./jquery"
 import "./jquery_ujs"
 import './dataTables/jquery.dataTables'
+
+$(document).ready(function(){
+  $(".dataTables_filter label input[type='search']").addClass('form-control')
+  $('#example_length label select').addClass('form-control')
+});
--- app/models/profile_admin.rb
+++ app/models/profile_admin.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class ProfileAdmin < ApplicationRecord
   belongs_to :admin
+  enum :role, %i[lawyer paralegal trainee secretary counter excounter]
 end
