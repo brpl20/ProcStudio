f3683bc58ba4f120ac4b1579852133a97887ee8c
Jemisson <jemison321@gmail.com>
2023-03-28 14:21:21 -0400

    Ajusta rubocop

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
@@ -52,44 +54,44 @@ end
 # For Rails apps, we'll make some of the shared paths that are shared between
 # all releases.
 task setup: :remote_environment do
-  queue! %[mkdir -p "#{deploy_to}/shared/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+  queue! %(mkdir -p "#{deploy_to}/shared/log")
+  queue! %(chmod g+rx,u+rwx "#{deploy_to}/shared/log")
 
-  queue! %[mkdir -p "#{deploy_to}/storage"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/storage"]
+  queue! %(mkdir -p "#{deploy_to}/storage")
+  queue! %(chmod g+rx,u+rwx "#{deploy_to}/storage")
 
-  queue! %[touch "#{deploy_to}/storage/index.html"]
+  queue! %(touch "#{deploy_to}/storage/index.html")
 
-  queue! %[mkdir -p "#{deploy_to}/shared/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+  queue! %(mkdir -p "#{deploy_to}/shared/config")
+  queue! %(chmod g+rx,u+rwx "#{deploy_to}/shared/config")
 
-  queue! %[mkdir -p "#{deploy_to}/shared/pids"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/pids"]
+  queue! %(mkdir -p "#{deploy_to}/shared/pids")
+  queue! %(chmod g+rx,u+rwx "#{deploy_to}/shared/pids")
 
-  queue! %[mkdir -p "#{deploy_to}/shared/tmp"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp"]
+  queue! %(mkdir -p "#{deploy_to}/shared/tmp")
+  queue! %(chmod g+rx,u+rwx "#{deploy_to}/shared/tmp")
 
-  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+  queue! %(touch "#{deploy_to}/shared/config/database.yml")
+  queue  %(echo "-----> Be sure to edit 'shared/config/database.yml'.")
 
-  queue! %[touch "#{deploy_to}/shared/config/application.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/application.yml'."]
+  queue! %(touch "#{deploy_to}/shared/config/application.yml")
+  queue  %(echo "-----> Be sure to edit 'shared/config/application.yml'.")
 
-  queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
+  queue! %(touch "#{deploy_to}/shared/config/secrets.yml")
+  queue  %(echo "-----> Be sure to edit 'shared/config/secrets.yml'.")
 end
 
 # Show logs
-desc "Show logs rails."
-task :'logs:rails' => :remote_environment do
+desc 'Show logs rails.'
+task 'logs:rails': :remote_environment do
   queue 'echo "Contents of the log file are as follows:"'
   queue "tail -f #{deploy_to}/shared/log/production.log"
 end
 
-desc "Show logs Nginx."
-task :'logs:nginx' => :remote_environment do
+desc 'Show logs Nginx.'
+task 'logs:nginx': :remote_environment do
   queue 'echo "Contents of the log file are as follows:"'
-  queue "tail -f /opt/nginx/logs/error.log"
+  queue 'tail -f /opt/nginx/logs/error.log'
 end
 
 desc 'Deploys the current version to the server.'
@@ -109,36 +111,36 @@ task deploy: :remote_environment do
 end
 
 # Roolback
-desc "Rolls back the latest release"
-task :rollback => :remote_environment do
-  queue! %[echo "-----> Rolling back to previous release for instance: #{domain}"]
+desc 'Rolls back the latest release'
+task rollback: :remote_environment do
+  queue! %(echo "-----> Rolling back to previous release for instance: #{domain}")
 
   # Delete existing sym link and create a new symlink pointing to the previous release
-  queue %[echo -n "-----> Creating new symlink from the previous release: "]
-  queue %[ls "#{deploy_to}/releases" -Art | sort | tail -n 2 | head -n 1]
-  queue! %[ls -Art "#{deploy_to}/releases" | sort | tail -n 2 | head -n 1 | xargs -I active ln -nfs "#{deploy_to}/releases/active" "#{deploy_to}/current"]
+  queue %(echo -n "-----> Creating new symlink from the previous release: ")
+  queue %(ls "#{deploy_to}/releases" -Art | sort | tail -n 2 | head -n 1)
+  queue! %(ls -Art "#{deploy_to}/releases" | sort | tail -n 2 | head -n 1 | xargs -I active ln -nfs "#{deploy_to}/releases/active" "#{deploy_to}/current")
 
   # Remove latest release folder (active release)
-  queue %[echo -n "-----> Deleting active release: "]
-  queue %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1]
-  queue! %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1 | xargs -I active rm -rf "#{deploy_to}/releases/active"]
+  queue %(echo -n "-----> Deleting active release: ")
+  queue %(ls "#{deploy_to}/releases" -Art | sort | tail -n 1)
+  queue! %(ls "#{deploy_to}/releases" -Art | sort | tail -n 1 | xargs -I active rm -rf "#{deploy_to}/releases/active")
 
-  queue %[echo -n "-----> Creating new restart.txt: "]
+  queue %(echo -n "-----> Creating new restart.txt: ")
   queue "touch #{deploy_to}/shared/tmp/restart.txt"
 end
 
 # Maintenance
 # TornOff (Necessary gem https://github.com/biola/turnout)
-desc "TurnOff"
-task :'system:turnoff' => :remote_environment do
-  queue %[echo -n "-----> Turn Off System: "]
-  queue! %[cd "#{deploy_to}/current"]
+desc 'TurnOff'
+task 'system:turnoff': :remote_environment do
+  queue %(echo -n "-----> Turn Off System: ")
+  queue! %(cd "#{deploy_to}/current")
   queue "RAILS_ENV=#{rails_env} bundle exec rake maintenance:start"
 end
 
-desc "TurnOn"
-task :'system:turnon' => :remote_environment do
-  queue %[echo -n "-----> Turn Off System: "]
-  queue! %[cd "#{deploy_to}/current"]
+desc 'TurnOn'
+task 'system:turnon': :remote_environment do
+  queue %(echo -n "-----> Turn Off System: ")
+  queue! %(cd "#{deploy_to}/current")
   queue "RAILS_ENV=#{rails_env} bundle exec rake maintenance:end"
 end
