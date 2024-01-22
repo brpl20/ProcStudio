bf1cb218de42c7957335a5afda74c552f53b2ef6
mattso <matts.ror@gmail.com>
2024-01-22 07:50:18 -0400

    PROC-66 | feat: adds polymorphic relation

--- app/models/represent.rb
+++ app/models/represent.rb
@@ -8,9 +8,9 @@
 #  profile_customer_id :bigint(8)        not null
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
-#  profile_admin_id    :bigint(8)        not null
+#  representor_id      :bigint(8)
 #
 class Represent < ApplicationRecord
   belongs_to :profile_customer
-  belongs_to :profile_admin
+  belongs_to :representor, class_name: 'ProfileCustomer', optional: true, foreign_key: 'representor_id'
 end
--- spec/models/represent_spec.rb
+++ spec/models/represent_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe Represent, type: :model do
         profile_customer_id: nil,
         created_at: nil,
         updated_at: nil,
-        profile_admin_id: nil
+        representor_id: nil
       )
     }
   end
 
   context 'Relations' do
     it { is_expected.to belong_to(:profile_customer) }
-    it { is_expected.to belong_to(:profile_admin) }
+    it { is_expected.to belong_to(:representor).class_name('ProfileCustomer').optional }
   end
 end
