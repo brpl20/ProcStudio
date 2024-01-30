b9869c16c6b436823316cc4eb9e458e03b7d01f8
mattso <matts.ror@gmail.com>
2024-01-30 14:36:39 -0400

    PROC-76 | feat: add base policies & specs

--- app/policies/admin/base_policy.rb
+++ app/policies/admin/base_policy.rb
@@ -5,9 +5,9 @@ class Admin::BasePolicy < ApplicationPolicy
     @role ||= user.profile_admin.role
   end
 
-  ProfileAdmin.roles.keys.each do |role|
+  ProfileAdmin.roles.each_key do |role|
     define_method("#{role}?") do # def lawyer?
       self.role == role          #  self.role == 'lawyer'
-    end                          # end
+    end
   end
 end
---
+++ app/policies/admin/customer_policy.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Admin::CustomerPolicy < Admin::BasePolicy
+  def index?
+    lawyer?
+  end
+
+  def show?
+    lawyer?
+  end
+
+  def create?
+    lawyer?
+  end
+
+  def update?
+    lawyer?
+  end
+
+  def destroy?
+    lawyer?
+  end
+end
