dbf9c1f75dc517a06cddde3f6565bd077fae341e
mattso <matts.ror@gmail.com>
2024-02-05 08:53:32 -0400

    PROC-65 | test: adds work_policy specs

--- app/policies/admin/work_policy.rb
+++ app/policies/admin/work_policy.rb
@@ -2,19 +2,19 @@
 
 class Admin::WorkPolicy < Admin::BasePolicy
   def index?
-    lawyer? || paralegal? || trainee? || secretary? || excounter?
+    lawyer? || paralegal? || trainee? || secretary? || counter? || excounter?
   end
 
   def show?
-    lawyer? || paralegal? || trainee? || secretary? || excounter?
+    lawyer? || paralegal? || trainee? || secretary? || counter? || excounter?
   end
 
   def create?
-    lawyer? || paralegal? || trainee? || secretary?
+    lawyer? || paralegal? || trainee? || secretary? || counter?
   end
 
   def update?
-    lawyer? || paralegal? || trainee? || secretary?
+    lawyer? || paralegal? || trainee? || secretary? || counter?
   end
 
   def destroy?
--- spec/policies/admin/work_policy_spec.rb
+++ spec/policies/admin/work_policy_spec.rb
@@ -6,33 +6,145 @@ RSpec.describe Admin::WorkPolicy, type: :policy do
   let(:admin) { build(:profile_admin).admin }
   subject { described_class }
 
-  permissions :index? do
-    describe 'when admin is lawyer' do
+  describe 'when admin is lawyer' do
+    permissions :index? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :show? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :create? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :update? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :destroy? do
       it { is_expected.to permit(admin, nil) }
     end
   end
 
-  permissions :show? do
-    describe 'when admin is lawyer' do
+  describe 'when admin is paralegal' do
+    before { admin.profile_admin.role = :paralegal }
+
+    permissions :index? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :show? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :create? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :update? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :destroy? do
       it { is_expected.to permit(admin, nil) }
     end
   end
 
-  permissions :create? do
-    describe 'when admin is lawyer' do
+  describe 'when admin is trainee' do
+    before { admin.profile_admin.role = :trainee }
+
+    permissions :index? do
       it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :show? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :create? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :update? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :destroy? do
+      it { is_expected.not_to permit(admin, nil) }
     end
   end
 
-  permissions :update? do
-    describe 'when admin is lawyer' do
+  describe 'when admin is secretary' do
+    before { admin.profile_admin.role = :secretary }
+
+    permissions :index? do
       it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :show? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :create? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :update? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :destroy? do
+      it { is_expected.not_to permit(admin, nil) }
     end
   end
 
-  permissions :destroy? do
-    describe 'when admin is lawyer' do
+  describe 'when admin is counter' do
+    before { admin.profile_admin.role = :counter }
+
+    permissions :index? do
       it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :show? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :create? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :update? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :destroy? do
+      it { is_expected.not_to permit(admin, nil) }
+    end
+  end
+
+  describe 'when admin is excounter' do
+    before { admin.profile_admin.role = :excounter }
+
+    permissions :index? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :show? do
+      it { is_expected.to permit(admin, nil) }
+    end
+
+    permissions :create? do
+      it { is_expected.not_to permit(admin, nil) }
+    end
+
+    permissions :update? do
+      it { is_expected.not_to permit(admin, nil) }
+    end
+
+    permissions :destroy? do
+      it { is_expected.not_to permit(admin, nil) }
     end
   end
 end
