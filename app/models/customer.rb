3e962108771ea646da85d050f88a7e5756e7be11
mattso <matts.ror@gmail.com>
2024-02-07 07:09:07 -0400

    PROC-6 | feat: add delegation to profile full name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -24,4 +24,6 @@ class Customer < ApplicationRecord
 
   has_one :profile_customer, dependent: :destroy
   has_many :jobs
+
+  delegate :full_name, to: :profile_customer, prefix: true, allow_nil: true
 end
