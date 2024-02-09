0de700bf421a328bb1536da8591a27586f4669b5
mattso <matts.ror@gmail.com>
2024-02-09 09:37:49 -0400

    PROC-87 | refact: changes jwt auth
    this commit changes jwt_auth so we reuse code between resources

--- app/controllers/concerns/jwt_auth.rb
+++ app/controllers/concerns/jwt_auth.rb
@@ -2,35 +2,40 @@
 
 module JwtAuth
   def authenticate_admin
-    token = request.headers['Authorization']&.split(' ')&.last
-    payload, _header = decode_token(token)
+    head :unauthorized and return unless payload.key?('admin_id')
 
     @current_admin ||= Admin.find(payload['admin_id'])
     head :unauthorized unless @current_admin&.jwt_token == token
-    head :unauthorized unless valid_token?(token)
+    head :unauthorized unless valid_token?
   rescue JWT::DecodeError
     head :unauthorized
   end
 
   def authenticate_customer
-    token = request.headers['Authorization']&.split(' ')&.last
-    payload, _header = decode_token(token)
+    head :unauthorized and return unless payload.key?('customer_id')
 
-    @current_customer ||= customer.find(payload['customer_id'])
+    @current_customer ||= Customer.find(payload['customer_id'])
     head :unauthorized unless @current_customer&.jwt_token == token
-    head :unauthorized unless valid_token?(token)
+    head :unauthorized unless valid_token?
   rescue JWT::DecodeError
     head :unauthorized
   end
 
   private
 
-  def decode_token(token)
-    JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256')
+  def token
+    @token ||= request.headers['Authorization']&.split(' ')&.last
   end
 
-  def valid_token?(token)
-    payload, _header = decode_token(token)
+  def decode_token
+    JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256')[0]
+  end
+
+  def payload
+    @payload ||= decode_token
+  end
+
+  def valid_token?
     Time.now.to_i < payload['exp']
   rescue JWT::DecodeError
     false
