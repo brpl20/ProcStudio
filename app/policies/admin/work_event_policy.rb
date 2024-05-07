ab62115591eee54cb4b75c309b4e5dd299466dc0
mattso <matts.ror@gmail.com>
2024-05-07 08:02:49 -0400

    PROC-160 | feat: add WorkEventPolicy

---
+++ app/policies/admin/work_event_policy.rb
@@ -0,0 +1,21 @@
+class Admin::WorkEventPolicy < Admin::BasePolicy
+  def index?
+    lawyer? || paralegal? || trainee? || secretary? || counter? || excounter?
+  end
+
+  def show?
+    index?
+  end
+
+  def create?
+    lawyer? || paralegal? || trainee? || secretary? || counter?
+  end
+
+  def update?
+    lawyer? || paralegal? || counter? || (trainee? && owner?) || (secretary? && owner?)
+  end
+
+  def destroy?
+    lawyer? || paralegal? || secretary?
+  end
+end
