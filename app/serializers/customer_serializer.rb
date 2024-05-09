785044dfb0449002f80eb64343f4e554ce589102
mattso <matts.ror@gmail.com>
2024-05-09 06:45:20 -0400

    PROC-164 | feat: add #profile_customer_id to CustomerSerializer

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -2,7 +2,7 @@
 
 class CustomerSerializer
   include JSONAPI::Serializer
-  attributes :email, :created_by_id, :confirmed_at
+  attributes :email, :created_by_id, :confirmed_at, :profile_customer_id
 
   attribute :confirmed, &:confirmed?
 end
