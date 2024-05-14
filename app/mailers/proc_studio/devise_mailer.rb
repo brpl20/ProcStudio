fd43ed467a65209c363e9ce99191a8160dc9ddc3
mattso <matts.ror@gmail.com>
2024-05-14 05:18:35 -0400

    PROC-168 | feat: configure reset instructions mailer

--- app/mailers/proc_studio/devise_mailer.rb
+++ app/mailers/proc_studio/devise_mailer.rb
@@ -11,4 +11,11 @@ class ProcStudio::DeviseMailer < Devise::Mailer
     confirmation_url = base_url + confirmation_path(token)
     Customers::Mail::WelcomeService.call(record, confirmation_url)
   end
+
+  def reset_password_instructions(record, token, _opts = {})
+    return if Rails.env.test?
+
+    password_url = base_url + update_password_path(token)
+    Customers::Mail::ResetPasswordService.call(record, password_url)
+  end
 end
