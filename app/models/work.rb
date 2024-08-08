eadd2f6f387230c4ae4589b95db2caa0daa06cfd
mattso <matts.ror@gmail.com>
2024-08-08 06:34:02 -0400

    refact: remove paranoid deletion from customer works

--- app/models/work.rb
+++ app/models/work.rb
@@ -57,7 +57,7 @@ class Work < ApplicationRecord
 
   has_many_attached :tributary_files
 
-  has_one :honorary
+  has_one :honorary, dependent: :destroy
   has_one :draft_work, class_name: 'Draft::Work', dependent: :destroy
 
   enum procedure: {
