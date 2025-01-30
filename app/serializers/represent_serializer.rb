ae0e0a01478c43f97eafc72c3fbffd98446a53c8
Carlos Moura <m.informatica@yahoo.com.br>
2025-01-30 12:11:29 -0300

    fix: RepresentSerializer identation

--- app/serializers/represent_serializer.rb
+++ app/serializers/represent_serializer.rb
@@ -1,6 +1,6 @@
 class RepresentSerializer
-    include JSONAPI::Serializer
+  include JSONAPI::Serializer
 
-    # Defina apenas os atributos que você quer incluir no retorno
-    attributes :id, :profile_customer_id, :representor_id
-  end
+  # Defina apenas os atributos que você quer incluir no retorno
+  attributes :id, :profile_customer_id, :representor_id
+end
