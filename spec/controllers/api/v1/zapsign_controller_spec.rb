318353688ff913b03f7aff57af530e392e16cc85
Carlos Moura <m.informatica@yahoo.com.br>
2025-02-28 18:16:44 -0300

    test: fix credentials mock

--- spec/controllers/api/v1/zapsign_controller_spec.rb
+++ spec/controllers/api/v1/zapsign_controller_spec.rb
@@ -17,8 +17,7 @@ RSpec.describe Api::V1::ZapsignController, type: :request do
       api_token: 'fake-api-token'
     )
 
-    # Mock para o secret_key
-    allow(Rails.application.credentials).to receive(:dig).with(:secret_key).and_return('valid_secret_key')
+    allow(Rails.application.credentials).to receive(:[]).with(:secret_key).and_return('valid_secret_key')
   end
 
   describe 'POST #create' do
